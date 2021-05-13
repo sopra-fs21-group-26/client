0e4a981e8085271ffa3b94bee7325b564235383c
Maximilian Jonescu <maximilian.jonescu@uzh.ch>
2021-05-13 16:39:17 +0200

    Added Volume Slider to Soundtrack
    - Fixed soundfile status change

--- src/components/home/Soundtrack.js
+++ src/components/home/Soundtrack.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Sound from 'react-sound';
 import soundtrack from '../home/soundtrack.mp3';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import {BsVolumeUp} from 'react-icons/bs';
+import {BsVolumeDown} from 'react-icons/bs';
+import Slider from '@material-ui/core/Slider';
 
 const PlayButton = styled.div`
 
@@ -13,7 +18,7 @@ const PlayButton = styled.div`
     font-family: Cornerstone;
     font-style: normal;
     font-weight: normal;
-    font-size: 2.33vh;
+    font-size: 1.88vh;
     color: #F2AD43;
     
     text-shadow: 0px 8px 4px rgba(0, 0, 0, 0.25);
@@ -22,39 +27,90 @@ const PlayButton = styled.div`
 
 const PlayButtonWrapper = styled.div`
     
-    width: 18%;
-    
-    position: absolute;
-    top: 2.7vh;
-    right: 0vw;
-    
     display: flex;
     justify-content: center;
     align-items: center;
 `;
 
-const Soundtrack = (
-    handleSongLoading,
-    handleSongPlaying,
-    handleSongFinishedPlaying
-) => {
+const SliderWrapper = styled.div`
+    
+    width: 65%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    position: relative;
+    bottom: 15%;
+`;
+
+const SliderWrapper2 = styled.div`
+    
+    width: 65%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+const Wrapper = styled.div`
+    
+    position: absolute;
+    right: 0vw;
+    top: 1vh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    height: 12vh;
+    width: 13vw;
+    
+`;
+
+const VolumeUp = styled(BsVolumeUp)`
+    color: #F2AD43;
+`;
+
+const VolumeDown = styled(BsVolumeDown)`
+    color: #F2AD43;
+`;
+
+const useStyles = makeStyles({
+    slider: {
+        color: '#F2AD43',
+    },
+});
+
+const Soundtrack = () => {
+
     const [isPlaying, setIsPlaying] = useState(false);
+    const classes = useStyles();
+    const [value, setValue] = useState(30);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return(
         <div>
-            <PlayButtonWrapper>
-                <PlayButton onClick={() => setIsPlaying(!isPlaying)}>{!isPlaying ? '<< Play Soundtrack >>' : '<< Stop Soundtrack >>'}</PlayButton>
-            </PlayButtonWrapper>
-            <Sound
-                url={soundtrack}
-                playStatus={
-                    isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED
-                }
-                playFromPosition={300}
-                onLoading={handleSongLoading}
-                onPlaying={handleSongPlaying}
-                onFinishedPLaying={handleSongFinishedPlaying}
-            />
+            <Wrapper>
+                <PlayButtonWrapper>
+                    <PlayButton onClick={() => setIsPlaying(!isPlaying)}>{!isPlaying ? '<< Play Soundtrack >>' : '<< Pause Soundtrack >>'}</PlayButton>
+                </PlayButtonWrapper>
+                <Sound
+                    volume={value}
+                    url={soundtrack}
+                    playStatus={
+                        isPlaying ? Sound.status.PLAYING : Sound.status.PAUSED
+                    }
+                />
+                <SliderWrapper>
+                    <VolumeDown/>
+                    <SliderWrapper2>
+                        <Slider className={classes.slider} value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
+                    </SliderWrapper2>
+                    <VolumeUp/>
+                </SliderWrapper>
+            </Wrapper>
         </div>
     )
 }
